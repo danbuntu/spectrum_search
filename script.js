1cdc13e1149d39766340d4cd89afd8d422588fb6
dan <danatwood@gmail.com>
2016-05-22 13:46:47 +0100

    split settings - started new search page

--- script.js
+++ script.js
@@ -1,172 +1,36 @@
-var url = 'http://search.kolbeck.dk/sinclair/game/',
-  imagesUrl = 'https://wos.meulie.net',
+$(document).ready(function() {
   id = getUrlParameter('id');
-settings = {
-  "settings": [{
-    "title": "fulltitle",
-    "label": "Full Title",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "machinetype",
-    "label": "Machine Type",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "yearofrelease",
-    "label": "Year Of Release",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "othersystems",
-    "label": "Other Systems",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "publisher",
-    "label": "Publisher",
-    "type": "setSimpleTable",
-    "debug": "0"
-  }, {
-    "title": "rereleasedby",
-    "label": "Re Released By",
-    "type": "setMultiRowTable",
-    "debug": "0"
-  }, {
-    "title": "additional",
-    "label": "Additional",
-    "type": "setMultiRowTable",
-    "debug": "0"
-  }, {
-    "title": "additionals",
-    "label": "Additionals",
-    "type": "setMultiRowTableAdditionals",
-    "debug": "0"
-  }, {
-    "title": "authors",
-    "label": "Authors",
-    "type": "setString",
-    "debug": "0"
-  }, {
-    "title": "availability",
-    "label": "Availability",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "budgetprice",
-    "label": "Budget Price",
-    "type": "setMultiRowTable",
-    "debug": "0"
-  }, {
-    "title": "controls",
-    "label": "Controls",
-    "type": "setString",
-    "debug": "0"
-  }, {
-    "title": "downloads",
-    "label": "Downloads",
-    "type": "setMultiRowTableDownloads",
-    "debug": "0"
-  }, {
-    "title": "magazine_0",
-    "label": "Magazine 0",
-    "type": "setMultiRowTableMagazine",
-    "debug": "0"
-  }, {
-    "title": "magazine_1",
-    "label": "Magazine 1",
-    "type": "setMultiRowTableMagazine",
-    "debug": "0"
-  }, {
-    "title": "messagelanguage",
-    "label": "Message Language",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "numberofplayers",
-    "label": "Number of Players",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "originalprice",
-    "label": "Original Price",
-    "type": "setMultiRowTable",
-    "debug": "0"
-  }, {
-    "title": "originalpublication",
-    "label": "Original Publication",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "othersystems",
-    "label": "Other Systems",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "protectionscheme",
-    "label": "Protection Scheme",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "pageinfo",
-    "label": "Page Info",
-    "type": "setSimpleTable",
-    "debug": "0"
-  }, {
-    "title": "score",
-    "label": "Score",
-    "type": "setSimpleTable",
-    "debug": "0"
-  }, {
-    "title": "series",
-    "label": "Series",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "tieinlicence",
-    "label": "Tie in Licence",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }, {
-    "title": "type",
-    "label": "Type",
-    "type": "setContentWithDivName",
-    "debug": "0"
-  }],
-};
 
-console.log(id);
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: url + id,
+    success: function(data) {
+      // console.log(data);
 
-$.ajax({
-  type: "GET",
-  dataType: "json",
-  url: url + id,
-  success: function(data) {
-    // console.log(data);
+      $('#loading').hide();
+      // start looping the _source object that contians everything
+      $.each(data['_source'], function(name, value) {
+        // search the settings for the right one
 
-    $('#loading').hide();
-    // start looping the _source object that contians everything
-    $.each(data['_source'], function(name, value) {
-      // search the settings for the right one
+        result = $.grep(settings.settings, function(element, index) {
+          return element.title == name;
+        });
 
-      result = $.grep(settings.settings, function(element, index) {
-        return element.title == name;
-      });
+        // check if the array is empty so we don't try and output it
+        if (result.length != 0) {
+          if (result[0].debug == 1) {
+            console.log(result);
+            console.log(value);
 
-      // check if the array is empty so we don't try and output it
-      if (result.length != 0) {
-        if (result[0].debug == 1) {
-          console.log(result);
-          console.log(value);
-
+          }
+          // call the correct function and pass it data
+          functions[result[0].type](result[0].title, result[0].label, value, result[0].debug);
         }
-        // call the correct function and pass it data
-        functions[result[0].type](result[0].title, result[0].label, value, result[0].debug);
-      }
-    });
-  }
+      });
+    }
+  });
 });
-
 
 // a method that contains the various functions we need to output
 var functions = {
---
+++ settings.js
@@ -0,0 +1,135 @@
+var url = 'http://search.kolbeck.dk/sinclair/game/',
+  imagesUrl = 'https://wos.meulie.net',
+settings = {
+  "settings": [{
+    "title": "fulltitle",
+    "label": "Full Title",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "machinetype",
+    "label": "Machine Type",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "yearofrelease",
+    "label": "Year Of Release",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "othersystems",
+    "label": "Other Systems",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "publisher",
+    "label": "Publisher",
+    "type": "setSimpleTable",
+    "debug": "0"
+  }, {
+    "title": "rereleasedby",
+    "label": "Re Released By",
+    "type": "setMultiRowTable",
+    "debug": "0"
+  }, {
+    "title": "additional",
+    "label": "Additional",
+    "type": "setMultiRowTable",
+    "debug": "0"
+  }, {
+    "title": "additionals",
+    "label": "Additionals",
+    "type": "setMultiRowTableAdditionals",
+    "debug": "0"
+  }, {
+    "title": "authors",
+    "label": "Authors",
+    "type": "setString",
+    "debug": "0"
+  }, {
+    "title": "availability",
+    "label": "Availability",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "budgetprice",
+    "label": "Budget Price",
+    "type": "setMultiRowTable",
+    "debug": "0"
+  }, {
+    "title": "controls",
+    "label": "Controls",
+    "type": "setString",
+    "debug": "0"
+  }, {
+    "title": "downloads",
+    "label": "Downloads",
+    "type": "setMultiRowTableDownloads",
+    "debug": "0"
+  }, {
+    "title": "magazine_0",
+    "label": "Magazine 0",
+    "type": "setMultiRowTableMagazine",
+    "debug": "0"
+  }, {
+    "title": "magazine_1",
+    "label": "Magazine 1",
+    "type": "setMultiRowTableMagazine",
+    "debug": "0"
+  }, {
+    "title": "messagelanguage",
+    "label": "Message Language",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "numberofplayers",
+    "label": "Number of Players",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "originalprice",
+    "label": "Original Price",
+    "type": "setMultiRowTable",
+    "debug": "0"
+  }, {
+    "title": "originalpublication",
+    "label": "Original Publication",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "othersystems",
+    "label": "Other Systems",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "protectionscheme",
+    "label": "Protection Scheme",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "pageinfo",
+    "label": "Page Info",
+    "type": "setSimpleTable",
+    "debug": "0"
+  }, {
+    "title": "score",
+    "label": "Score",
+    "type": "setSimpleTable",
+    "debug": "0"
+  }, {
+    "title": "series",
+    "label": "Series",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "tieinlicence",
+    "label": "Tie in Licence",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }, {
+    "title": "type",
+    "label": "Type",
+    "type": "setContentWithDivName",
+    "debug": "0"
+  }],
+};
